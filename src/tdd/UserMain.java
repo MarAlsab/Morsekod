3b7776d0ae9697d64b9d5932c43947eca3fc179c
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2024-10-24 12:05:33 +0200

    User

---
+++ src/tdd/UserMain.java
@@ -0,0 +1,22 @@
+package tdd;
+
+import java.util.Scanner;
+
+public class UserMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        int antalRader = 0;
+        do {
+            antalRader++;
+            System.out.print("Skriv in användarnamn och lösenord ");
+            line = scanner.nextLine();
+            System.out.println("Ditt anmvändarnamn är " + line);
+            int sumAntalTecken = 0;
+            sumAntalTecken = line.length();
+            System.out.println(sumAntalTecken);
+            //if (line.contains("stop")) System.out.println("Tack och hej");
+        }while (! line.contains("stop"));
+        System.out.println(antalRader);
+    }
+}
---
+++ src/tdd/UserTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+    @Test
+    void testOfUser(){
+        User user = new User("tomas","abc123");
+    }
+}
