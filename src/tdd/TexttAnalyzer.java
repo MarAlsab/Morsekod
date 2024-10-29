8be25f19ebc31abb3bf653f53c82630316b0cfbb
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2024-10-29 09:25:19 +0100

    Tesxanalyzer draft

---
+++ src/tdd/TexttAnalyzer.java
@@ -0,0 +1,17 @@
+package tdd;
+
+public class TexttAnalyzer {
+    private int rows;
+
+    public TexttAnalyzer() {
+        rows = 0;
+    }
+
+    public void lineInput(String line) {
+        rows++;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+}
