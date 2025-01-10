5f4d3f8bdc4cf8880304518ec8cfd1d79dc3dc42
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-10 10:10:26 +0100

    IntegerArray part 2

--- src/automation/ovning1extra/IntegerArray.java
+++ src/automation/ovning1extra/IntegerArray.java
@@ -1,20 +1,33 @@
 package automation.ovning1extra;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // 1. Skriv ett program som läser in heltal med Scanner-metoden nextInt().
 // Fånga InputMismatchException om användaren skriver något som inte är ett heltal
 
+// 2. Läs in fem heltal och lägg dem i en array. Se till att inläsningen pågår
+//  tills det att användaren har fyllt i fem heltal.
+// Användaren ska få ett tydligt felmeddelande om den skriver fel.
+
 public class IntegerArray {
     public static void main(String[] args) {
         int number = 0;
-        try {
-            number = readInt();
-        } catch (InputMismatchException ime) {
-            System.out.println("Du skrev inget heltal");
+        int[] integerArray = new int[5];
+        for (int i = 0; i < 5; i++) {
+            while (true) {
+                try {
+                    number = readInt();
+                    break;
+                } catch (InputMismatchException ime) {
+                    System.out.println("Du skrev inget heltal");
+                }
+            }
+            System.out.println("Vårt inlästa nummer är: " + number);
+            integerArray[i] = number;
         }
-        System.out.println("Vårt inlästa nummer är: " + number);
+        System.out.println(Arrays.toString(integerArray));
     }
 
     private static int readInt() {
