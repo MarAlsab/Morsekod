16d394ea6efd79132e740d6149d00f1a7c20f741
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-21 09:44:47 +0100

    Arraylists

---
+++ src/automation/arraylist/WeekdaysTest.java
@@ -0,0 +1,21 @@
+package automation.arraylist;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WeekdaysTest {
+    @Test
+    public void monday(){
+        Weekdays weekdays = new Weekdays();
+        assertEquals("Monday",weekdays.dayOfWeek(1));
+    }
+
+    @Test
+    public void TestOfArrayList(){
+        Weekdays weekdays = new Weekdays();
+        assertEquals("Monday",weekdays.dayOfWeekArrayList(1));
+        assertEquals("Wednesday",weekdays.dayOfWeekArrayList(3));
+    }
+
+
+}
