a90ee4456a3e1d59c815491e69926ac22f1228ba
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-21 12:27:45 +0100

    Arraylists with names

--- src/automation/arraylist/MatematiskaKonstanter.java
+++ src/automation/arraylist/MatematiskaKonstanter.java
@@ -5,8 +5,8 @@ package automation.arraylist;
 //
 //Namn	Värde
 //pi		3.1415926535
-//e		2.7182818284
-//sqrt2	1.4142135623
+//e		    2.7182818284
+//sqrt2	    1.4142135623
 //phi		1.6180339887
 //ln10		2.3025850929
 
@@ -21,6 +21,16 @@ public class MatematiskaKonstanter {
         constants.add(1.6180339887);
         constants.add(2.3025850929);
         System.out.println(constants);
+
+        ArrayList<Double> constantsClone = (ArrayList<Double>) constants.clone();
+        constants.remove(constants.remove(constants.size() - 3));
+        System.out.println(constants.indexOf(2.7182818284));
+        System.out.println(constants);
+        System.out.println(constants.contains(3.1415926535));
+        constants.forEach(constant -> System.out.println(constant));
+        constants.clear();
+        System.out.println(constants);
+        System.out.println(constantsClone);
 
     }
 }
